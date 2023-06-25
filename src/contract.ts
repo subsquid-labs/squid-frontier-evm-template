2f448173dfe58c4a9c06fcffa8a8a594a5a2ebec
Alexander Belopashentsev <abelopashentsev@gmail.com>
2023-06-25 15:45:13 +0500

    bump evm packages and update src structure

--- src/contract.ts
+++ src/contract.ts
@@ -1,27 +1,25 @@
-import { Store } from "@subsquid/typeorm-store";
-import { Contract } from "./model";
-
-export const contractAddress = "0xb654611f84a8dc429ba3cb4fda9fad236c505a1a";
-export const MULTICALL_CONTRACT='0x83e3b61886770de2F64AAcaD2724ED4f08F7f36B'
+import {Store} from '@subsquid/typeorm-store'
+import {Contract} from './model'
+import {CONTRACT_ADDRESS} from './processor'
 
 export function createContractEntity(): Contract {
-  return new Contract({
-    id: contractAddress,
-    name: "Moonsama",
-    symbol: "MSAMA",
-    totalSupply: 1000n,
-  });
+    return new Contract({
+        id: CONTRACT_ADDRESS,
+        name: 'Moonsama',
+        symbol: 'MSAMA',
+        totalSupply: 1000n,
+    })
 }
 
-let contractEntity: Contract | undefined;
+let contractEntity: Contract | undefined
 
 export async function getContractEntity(store: Store): Promise<Contract> {
-  if (contractEntity == null) {
-    contractEntity = await store.get(Contract, contractAddress);
     if (contractEntity == null) {
-      contractEntity = createContractEntity();
-      await store.insert(contractEntity);
+        contractEntity = await store.get(Contract, CONTRACT_ADDRESS)
+        if (contractEntity == null) {
+            contractEntity = createContractEntity()
+            await store.insert(contractEntity)
+        }
     }
-  }
-  return contractEntity;
+    return contractEntity
 }
