2d70fb21814fece55f6a9d1adb1ec3cf6f4ff278
abernatskiy <abernatskiy@toha.li>
2024-06-25 21:06:21 +0900

    Updating the autogenerated ABI files
    	deleted:    src/abi/abi.support.ts
    	deleted:    src/abi/erc721.abi.ts
    	modified:   src/abi/erc721.ts

--- src/abi/erc721.ts
+++ src/abi/erc721.ts
@@ -1,122 +1,138 @@
-import * as ethers from 'ethers'
-import {LogEvent, Func, ContractBase} from './abi.support'
-import {ABI_JSON} from './erc721.abi'
-
-export const abi = new ethers.Interface(ABI_JSON);
+import * as p from '@subsquid/evm-codec'
+import { event, fun, viewFun, indexed, ContractBase } from '@subsquid/evm-abi'
+import type { EventParams as EParams, FunctionArguments, FunctionReturn } from '@subsquid/evm-abi'
 
 export const events = {
-    Approval: new LogEvent<([owner: string, approved: string, tokenId: bigint] & {owner: string, approved: string, tokenId: bigint})>(
-        abi, '0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925'
-    ),
-    ApprovalForAll: new LogEvent<([owner: string, operator: string, approved: boolean] & {owner: string, operator: string, approved: boolean})>(
-        abi, '0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31'
-    ),
-    Transfer: new LogEvent<([from: string, to: string, tokenId: bigint] & {from: string, to: string, tokenId: bigint})>(
-        abi, '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-    ),
+    Approval: event("0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925", "Approval(address,address,uint256)", {"owner": indexed(p.address), "approved": indexed(p.address), "tokenId": indexed(p.uint256)}),
+    ApprovalForAll: event("0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31", "ApprovalForAll(address,address,bool)", {"owner": indexed(p.address), "operator": indexed(p.address), "approved": p.bool}),
+    Transfer: event("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef", "Transfer(address,address,uint256)", {"from": indexed(p.address), "to": indexed(p.address), "tokenId": indexed(p.uint256)}),
 }
 
 export const functions = {
-    approve: new Func<[to: string, tokenId: bigint], {to: string, tokenId: bigint}, []>(
-        abi, '0x095ea7b3'
-    ),
-    balanceOf: new Func<[owner: string], {owner: string}, bigint>(
-        abi, '0x70a08231'
-    ),
-    baseURI: new Func<[], {}, string>(
-        abi, '0x6c0360eb'
-    ),
-    getApproved: new Func<[tokenId: bigint], {tokenId: bigint}, string>(
-        abi, '0x081812fc'
-    ),
-    isApprovedForAll: new Func<[owner: string, operator: string], {owner: string, operator: string}, boolean>(
-        abi, '0xe985e9c5'
-    ),
-    name: new Func<[], {}, string>(
-        abi, '0x06fdde03'
-    ),
-    ownerOf: new Func<[tokenId: bigint], {tokenId: bigint}, string>(
-        abi, '0x6352211e'
-    ),
-    'safeTransferFrom(address,address,uint256)': new Func<[from: string, to: string, tokenId: bigint], {from: string, to: string, tokenId: bigint}, []>(
-        abi, '0x42842e0e'
-    ),
-    'safeTransferFrom(address,address,uint256,bytes)': new Func<[from: string, to: string, tokenId: bigint, _data: string], {from: string, to: string, tokenId: bigint, _data: string}, []>(
-        abi, '0xb88d4fde'
-    ),
-    setApprovalForAll: new Func<[operator: string, approved: boolean], {operator: string, approved: boolean}, []>(
-        abi, '0xa22cb465'
-    ),
-    supportsInterface: new Func<[interfaceId: string], {interfaceId: string}, boolean>(
-        abi, '0x01ffc9a7'
-    ),
-    symbol: new Func<[], {}, string>(
-        abi, '0x95d89b41'
-    ),
-    tokenByIndex: new Func<[index: bigint], {index: bigint}, bigint>(
-        abi, '0x4f6ccce7'
-    ),
-    tokenOfOwnerByIndex: new Func<[owner: string, index: bigint], {owner: string, index: bigint}, bigint>(
-        abi, '0x2f745c59'
-    ),
-    tokenURI: new Func<[tokenId: bigint], {tokenId: bigint}, string>(
-        abi, '0xc87b56dd'
-    ),
-    totalSupply: new Func<[], {}, bigint>(
-        abi, '0x18160ddd'
-    ),
-    transferFrom: new Func<[from: string, to: string, tokenId: bigint], {from: string, to: string, tokenId: bigint}, []>(
-        abi, '0x23b872dd'
-    ),
+    approve: fun("0x095ea7b3", "approve(address,uint256)", {"to": p.address, "tokenId": p.uint256}, ),
+    balanceOf: viewFun("0x70a08231", "balanceOf(address)", {"owner": p.address}, p.uint256),
+    baseURI: viewFun("0x6c0360eb", "baseURI()", {}, p.string),
+    getApproved: viewFun("0x081812fc", "getApproved(uint256)", {"tokenId": p.uint256}, p.address),
+    isApprovedForAll: viewFun("0xe985e9c5", "isApprovedForAll(address,address)", {"owner": p.address, "operator": p.address}, p.bool),
+    name: viewFun("0x06fdde03", "name()", {}, p.string),
+    ownerOf: viewFun("0x6352211e", "ownerOf(uint256)", {"tokenId": p.uint256}, p.address),
+    "safeTransferFrom(address,address,uint256)": fun("0x42842e0e", "safeTransferFrom(address,address,uint256)", {"from": p.address, "to": p.address, "tokenId": p.uint256}, ),
+    "safeTransferFrom(address,address,uint256,bytes)": fun("0xb88d4fde", "safeTransferFrom(address,address,uint256,bytes)", {"from": p.address, "to": p.address, "tokenId": p.uint256, "_data": p.bytes}, ),
+    setApprovalForAll: fun("0xa22cb465", "setApprovalForAll(address,bool)", {"operator": p.address, "approved": p.bool}, ),
+    supportsInterface: viewFun("0x01ffc9a7", "supportsInterface(bytes4)", {"interfaceId": p.bytes4}, p.bool),
+    symbol: viewFun("0x95d89b41", "symbol()", {}, p.string),
+    tokenByIndex: viewFun("0x4f6ccce7", "tokenByIndex(uint256)", {"index": p.uint256}, p.uint256),
+    tokenOfOwnerByIndex: viewFun("0x2f745c59", "tokenOfOwnerByIndex(address,uint256)", {"owner": p.address, "index": p.uint256}, p.uint256),
+    tokenURI: viewFun("0xc87b56dd", "tokenURI(uint256)", {"tokenId": p.uint256}, p.string),
+    totalSupply: viewFun("0x18160ddd", "totalSupply()", {}, p.uint256),
+    transferFrom: fun("0x23b872dd", "transferFrom(address,address,uint256)", {"from": p.address, "to": p.address, "tokenId": p.uint256}, ),
 }
 
 export class Contract extends ContractBase {
 
-    balanceOf(owner: string): Promise<bigint> {
-        return this.eth_call(functions.balanceOf, [owner])
+    balanceOf(owner: BalanceOfParams["owner"]) {
+        return this.eth_call(functions.balanceOf, {owner})
     }
 
-    baseURI(): Promise<string> {
-        return this.eth_call(functions.baseURI, [])
+    baseURI() {
+        return this.eth_call(functions.baseURI, {})
     }
 
-    getApproved(tokenId: bigint): Promise<string> {
-        return this.eth_call(functions.getApproved, [tokenId])
+    getApproved(tokenId: GetApprovedParams["tokenId"]) {
+        return this.eth_call(functions.getApproved, {tokenId})
     }
 
-    isApprovedForAll(owner: string, operator: string): Promise<boolean> {
-        return this.eth_call(functions.isApprovedForAll, [owner, operator])
+    isApprovedForAll(owner: IsApprovedForAllParams["owner"], operator: IsApprovedForAllParams["operator"]) {
+        return this.eth_call(functions.isApprovedForAll, {owner, operator})
     }
 
-    name(): Promise<string> {
-        return this.eth_call(functions.name, [])
+    name() {
+        return this.eth_call(functions.name, {})
     }
 
-    ownerOf(tokenId: bigint): Promise<string> {
-        return this.eth_call(functions.ownerOf, [tokenId])
+    ownerOf(tokenId: OwnerOfParams["tokenId"]) {
+        return this.eth_call(functions.ownerOf, {tokenId})
     }
 
-    supportsInterface(interfaceId: string): Promise<boolean> {
-        return this.eth_call(functions.supportsInterface, [interfaceId])
+    supportsInterface(interfaceId: SupportsInterfaceParams["interfaceId"]) {
+        return this.eth_call(functions.supportsInterface, {interfaceId})
     }
 
-    symbol(): Promise<string> {
-        return this.eth_call(functions.symbol, [])
+    symbol() {
+        return this.eth_call(functions.symbol, {})
     }
 
-    tokenByIndex(index: bigint): Promise<bigint> {
-        return this.eth_call(functions.tokenByIndex, [index])
+    tokenByIndex(index: TokenByIndexParams["index"]) {
+        return this.eth_call(functions.tokenByIndex, {index})
     }
 
-    tokenOfOwnerByIndex(owner: string, index: bigint): Promise<bigint> {
-        return this.eth_call(functions.tokenOfOwnerByIndex, [owner, index])
+    tokenOfOwnerByIndex(owner: TokenOfOwnerByIndexParams["owner"], index: TokenOfOwnerByIndexParams["index"]) {
+        return this.eth_call(functions.tokenOfOwnerByIndex, {owner, index})
     }
 
-    tokenURI(tokenId: bigint): Promise<string> {
-        return this.eth_call(functions.tokenURI, [tokenId])
+    tokenURI(tokenId: TokenURIParams["tokenId"]) {
+        return this.eth_call(functions.tokenURI, {tokenId})
     }
 
-    totalSupply(): Promise<bigint> {
-        return this.eth_call(functions.totalSupply, [])
+    totalSupply() {
+        return this.eth_call(functions.totalSupply, {})
     }
 }
+
+/// Event types
+export type ApprovalEventArgs = EParams<typeof events.Approval>
+export type ApprovalForAllEventArgs = EParams<typeof events.ApprovalForAll>
+export type TransferEventArgs = EParams<typeof events.Transfer>
+
+/// Function types
+export type ApproveParams = FunctionArguments<typeof functions.approve>
+export type ApproveReturn = FunctionReturn<typeof functions.approve>
+
+export type BalanceOfParams = FunctionArguments<typeof functions.balanceOf>
+export type BalanceOfReturn = FunctionReturn<typeof functions.balanceOf>
+
+export type BaseURIParams = FunctionArguments<typeof functions.baseURI>
+export type BaseURIReturn = FunctionReturn<typeof functions.baseURI>
+
+export type GetApprovedParams = FunctionArguments<typeof functions.getApproved>
+export type GetApprovedReturn = FunctionReturn<typeof functions.getApproved>
+
+export type IsApprovedForAllParams = FunctionArguments<typeof functions.isApprovedForAll>
+export type IsApprovedForAllReturn = FunctionReturn<typeof functions.isApprovedForAll>
+
+export type NameParams = FunctionArguments<typeof functions.name>
+export type NameReturn = FunctionReturn<typeof functions.name>
+
+export type OwnerOfParams = FunctionArguments<typeof functions.ownerOf>
+export type OwnerOfReturn = FunctionReturn<typeof functions.ownerOf>
+
+export type SafeTransferFromParams_0 = FunctionArguments<typeof functions["safeTransferFrom(address,address,uint256)"]>
+export type SafeTransferFromReturn_0 = FunctionReturn<typeof functions["safeTransferFrom(address,address,uint256)"]>
+
+export type SafeTransferFromParams_1 = FunctionArguments<typeof functions["safeTransferFrom(address,address,uint256,bytes)"]>
+export type SafeTransferFromReturn_1 = FunctionReturn<typeof functions["safeTransferFrom(address,address,uint256,bytes)"]>
+
+export type SetApprovalForAllParams = FunctionArguments<typeof functions.setApprovalForAll>
+export type SetApprovalForAllReturn = FunctionReturn<typeof functions.setApprovalForAll>
+
+export type SupportsInterfaceParams = FunctionArguments<typeof functions.supportsInterface>
+export type SupportsInterfaceReturn = FunctionReturn<typeof functions.supportsInterface>
+
+export type SymbolParams = FunctionArguments<typeof functions.symbol>
+export type SymbolReturn = FunctionReturn<typeof functions.symbol>
+
+export type TokenByIndexParams = FunctionArguments<typeof functions.tokenByIndex>
+export type TokenByIndexReturn = FunctionReturn<typeof functions.tokenByIndex>
+
+export type TokenOfOwnerByIndexParams = FunctionArguments<typeof functions.tokenOfOwnerByIndex>
+export type TokenOfOwnerByIndexReturn = FunctionReturn<typeof functions.tokenOfOwnerByIndex>
+
+export type TokenURIParams = FunctionArguments<typeof functions.tokenURI>
+export type TokenURIReturn = FunctionReturn<typeof functions.tokenURI>
+
+export type TotalSupplyParams = FunctionArguments<typeof functions.totalSupply>
+export type TotalSupplyReturn = FunctionReturn<typeof functions.totalSupply>
+
+export type TransferFromParams = FunctionArguments<typeof functions.transferFrom>
+export type TransferFromReturn = FunctionReturn<typeof functions.transferFrom>
+
